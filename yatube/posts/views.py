a0662702a2de9ecefb26ae6e296bc6ee03e3224b
123 <abalnokin@mail.ru>
2022-09-08 12:58:52 +0300

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,12 +1,38 @@
 from django.shortcuts import render
+from .models import Post
 from django.http import HttpResponse
+from django.template import loader
 
 
 # Главная страница
 def index(request):
-    return HttpResponse('Главная страница')
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'templates/posts/index.html', context)
+
+
+
+
+def group_list(request):
+    template = 'templates/posts/group_list.html'
+    return render(request, template)
+
 
 # Страница с информацией об одном сорте мороженого;
 # view-функция принимает параметр pk из path()
 def group_posts(request, slug):
-    return HttpResponse(f'Группа с названием {slug}')
+    template = 'templates/posts/group_posts.html'
+    title = "Здесь будет информация о группах проекта Yatube"
+    text = 'Последние обновления на сайте'
+    context = {
+        'title': title,
+        'text': text,
+    }
+    return render(request, template, context)
+#    return HttpResponse(f'Группа с названием {slug}')
+
+
+#def group_posts(request, slug):
+#    return HttpResponse(f'Группа с названием {slug}')
